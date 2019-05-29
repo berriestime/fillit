bf44aa449ef3229324f193d34a7cbb424297b809
Selmond Elly <selly@at-n8.21-school.ru>
2019-05-29 16:47:36 +0300

    check tetrimino A & B

--- terminos_type.c
+++ terminos_type.c
@@ -6,32 +6,50 @@
 /*   By: selly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 11:57:43 by selly             #+#    #+#             */
-/*   Updated: 2019/05/29 15:51:52 by selly            ###   ########.fr       */
+/*   Updated: 2019/05/29 16:45:58 by selly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tetriminos.h"
 
-int		type_F(char *str) /*здесь будет проверка на 6 фигуру*/
+int		touch(char *str);
+
+int		type_F(char *str)
 {
 	return (0);
 }
 
-int		type_D(char *str) /*здесь будет проверка на 4 фигуру*/
+int		type_D(char *str)
 {
 	return (0);
 }
 
-int		type_C(char *str) /*здесь будет проверка на 3 фигуру*/ 
+int		type_C(char *str) 
 {
 	return (0);
 }
-int		type_B(char *str) /*здесь будет проверка на 2 фигуру*/
+int		type_B(char *str)
 {
 	int		i;
 	int		line;
 
 	line = 4;
 	i = touch(str);
+	if (i >= 0 && i < 13)
+	{
+		while (str[i] == '#')
+		{
+			line--;
+			i++;
+		}
+		if (line == 2)
+		{
+			i  += 3;
+			while (str[i++] == '#')
+				line--;
+		}
+	}
+	if (line == 0)
+		return (2);
 	return (-1);
 }
