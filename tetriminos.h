7ed1e73cd9eda82332d2ce86021912229aea3b45
Selmond Elly <selly@at-n8.21-school.ru>
2019-05-29 15:56:58 +0300

    tetriminos test

--- tetriminos.h
+++ tetriminos.h
@@ -6,7 +6,7 @@
 /*   By: dmorrige <dmorrige@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 08:21:13 by dmorrige          #+#    #+#             */
-/*   Updated: 2019/05/29 12:02:56 by dmorrige         ###   ########.fr       */
+/*   Updated: 2019/05/29 15:52:06 by selly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,13 @@
 # define MAX_SQUARE 16
 # define VALID_TTR_SYM_COUNT 20
 
-void	ft_tetr_validation(int fd, unsigned int tetr[MAX_TETRS + 1]);
+void			ft_tetr_validation(int fd, unsigned int tetr[MAX_TETRS + 1]);
 unsigned int	tetr_parser(char *a);
+
+int				termino(char *str);
+int				type_B(char *str);
+int				type_C(char *str);
+int				typr_D(char *str);
+int             typr_F(char *str);
 
 #endif
